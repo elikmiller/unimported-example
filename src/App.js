9864c8331aec00bda3f45555c2b5c04fd0580135
Eli Miller <emiller@specterops.io>
2021-08-03 10:56:27 -0700

    Add example scenario for unimported

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Example from "src/components/Example";
 
 function App() {
   return (
     <div className="App">
+      <Example />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
